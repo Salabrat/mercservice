5fccb22c16a77e39b0475de669edbd0c1fd434f9
Salabrat <salabrat@github.com>
2026-08-09 19:23:41 +0300

    Remove opacity from slide-down animation to fix transparency issue

--- tailwind.config.js
+++ tailwind.config.js
@@ -85,11 +85,9 @@ export default {
         },
         'slide-down': {
           '0%': {
-            opacity: '0',
             transform: 'translateY(-10px)',
           },
           '100%': {
-            opacity: '1',
             transform: 'translateY(0)',
           },
         },
