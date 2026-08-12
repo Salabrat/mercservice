7d413df56cc0cb7f9c5a5f2f517d8bf717bed19b
Salabrat <salabrat@github.com>
2026-08-12 19:27:34 +0300

    Center HeroHeader logo and use white version

--- src/components/HeroHeader.jsx
+++ src/components/HeroHeader.jsx
@@ -4,9 +4,9 @@ import { Link } from 'react-router-dom'
 const HeroHeader = () => {
   return (
     <header className="fixed top-0 left-0 right-0 z-20 px-6 py-4">
-      <div className="flex items-center justify-between">
+      <div className="flex items-center justify-center">
         <Link to="/" className="flex items-center">
-          <img src="/images/MAINLOGO.png" alt="MAINLOGO" className="h-12 object-contain" />
+          <img src="/images/MAINLOGO white.png" alt="MAINLOGO" className="h-12 object-contain" />
         </Link>
       </div>
     </header>
