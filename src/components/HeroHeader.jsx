39b5dcc0914cfa11549e3a012af9fa0aba9a7472
Salabrat <salabrat@github.com>
2026-08-12 19:28:21 +0300

    Change HeroHeader from fixed to absolute positioning to stay within Hero section

--- src/components/HeroHeader.jsx
+++ src/components/HeroHeader.jsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom'
 
 const HeroHeader = () => {
   return (
-    <header className="fixed top-0 left-0 right-0 z-20 px-6 py-4">
+    <header className="absolute top-0 left-0 right-0 z-30 px-6 py-4">
       <div className="flex items-center justify-center">
         <Link to="/" className="flex items-center">
           <img src="/images/MAINLOGO white.png" alt="MAINLOGO" className="h-12 object-contain" />
